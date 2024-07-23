fdaa6fb68e94ad8f6535e529c748cfc35641c144
aduchate <ad@icure.com>
2024-07-23 16:56:58 +0200

    Fix amp view on old couchdbs

--- sam/src/main/resources/org/taktik/icure/asyncdao/samv2/impl/js/amp/By_language_label.js
+++ sam/src/main/resources/org/taktik/icure/asyncdao/samv2/impl/js/amp/By_language_label.js
@@ -1,9 +1,11 @@
 map = function(doc) {
     var normalize_substrings = function(text,latin_map) {
-        return text.trim().split(/[ |/]+/).flatMap(function(word) {
+        var acc = [];
+        text.trim().split(/[ |/]+/).forEach(function(word) {
             var normalized = word.toLowerCase().replace(/[^A-Za-z0-9-]/g,function(a){return latin_map[a]||""});
-            return (normalized.includes("-") ? [normalized, normalized.replace('-','')].concat(normalized.split('-')) : [normalized]).filter((x) => x !== '-');
+            (normalized.includes("-") ? [normalized, normalized.replace('-','')].concat(normalized.split('-')) : [normalized]).filter((x) => x !== '-').forEach((x) => acc.push(x));
         });
+        return acc;
     };
 
     if (doc.java_type === 'org.taktik.icure.entities.samv2.Amp' && !doc.deleted) {
